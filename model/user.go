3b2e919e7114fb4b6766613c9356314ee80895c9
duanfeng <824780260@qq.com>
2022-10-06 20:18:52 +0700

    fix：修改字段

--- model/user.go
+++ model/user.go
@@ -7,5 +7,5 @@ type User struct {
 	Username   string `json:"username" validate:"required,min=4,max=12" label:"用户名"`
 	Password   string `json:"password" validate:"required,min=6,max=20" label:"密码"`
 	RoleId     int64  `json:"role" validate:"required" label:"角色ID"`
-	DeleteFlag int    `json:"delete_flag" label:"是否有效0-有效1-无效"`
+	Valid      int    `json:"Valid" label:"是否有效0-有效1-无效"`
 }
